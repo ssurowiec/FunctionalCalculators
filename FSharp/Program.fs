985862d859d74050f2185a8da27ef8062e423c3e
Szymon Surowiec <ssurowiec@cybervadis-dev.com>
2024-06-11 12:59:58 +0200

    Add result handling

--- FSharp/Program.fs
+++ FSharp/Program.fs
@@ -43,58 +43,89 @@ module Operator =
 
 printfn "Set calculation: "
 let calculation = Console.ReadLine()
-type Evaluation = int -> Operator -> int -> int
+type Evaluation = int -> Operator -> int -> Result<int, string>
 
 module Evaluation =
     let compute: Evaluation =
         fun a op b ->
             match op with
-            | Add -> a + b
-            | Subtract -> a - b
-            | Multiply -> a * b
-            | Divide -> a / b
-            | Power -> pown a b
+            | Add -> (a + b) |> Ok
+            | Subtract -> (a - b) |> Ok
+            | Multiply -> (a * b) |> Ok
+            | Divide ->
+                match b = 0 with
+                | true -> Error "Cannot divide by zero"
+                | false -> (a / b) |> Ok
+            | Power -> (pown a b) |> Ok
 
 let rec computeCalculation (rawCalculation: string) =
     let splitComputation = rawCalculation.Replace(" ", String.Empty)
 
-    let rec computeEvaluation (numberBuffer: string) (prevEval: int -> int) (c: char list) =
+    let rec computeEvaluation (numberBuffer: string) (prevEval: int -> Result<int, string>) (c: char list) =
         match c with
         | Operator.Signs.Add :: rest ->
             let number = numberBuffer |> int
-            let calculation = Evaluation.compute (prevEval number) Operator.Add
-            computeEvaluation String.Empty calculation rest
+
+            let ev =
+                fun x -> prevEval number |> Result.bind (fun r -> Evaluation.compute r Operator.Add x)
+
+            computeEvaluation String.Empty ev rest
         | Operator.Signs.Subtract :: rest ->
             let number = numberBuffer |> int
-            let ev = Evaluation.compute (prevEval number) Operator.Subtract
+
+            let ev =
+                fun x ->
+                    prevEval number
+                    |> Result.bind (fun r -> Evaluation.compute r Operator.Subtract x)
+
             computeEvaluation String.Empty ev rest
         | Operator.Signs.Multiply :: rest ->
             let number = numberBuffer |> int
-            let ev = Evaluation.compute (prevEval number) Operator.Multiply
+
+            let ev =
+                fun x ->
+                    prevEval number
+                    |> Result.bind (fun r -> Evaluation.compute r Operator.Multiply x)
+
             computeEvaluation String.Empty ev rest
         | Operator.Signs.Divide :: rest ->
             let number = numberBuffer |> int
-            let ev = Evaluation.compute (prevEval number) Operator.Divide
+
+            let ev =
+                fun x -> prevEval number |> Result.bind (fun r -> Evaluation.compute r Operator.Divide x)
+
             computeEvaluation String.Empty ev rest
         | Operator.Signs.Power :: rest ->
             let number = numberBuffer |> int
-            let ev = Evaluation.compute (prevEval number) Operator.Power
+
+            let ev =
+                fun x -> prevEval number |> Result.bind (fun r -> Evaluation.compute r Operator.Power x)
+
             computeEvaluation String.Empty ev rest
         | Operator.Signs.OpenBracket :: rest ->
             let nestedCalculation = computeCalculation (String.Join(String.Empty, rest))
 
-            rest
-            |> List.findIndex (fun c -> c = Operator.Signs.CloseBracket)
-            |> (fun index -> rest |> List.splitAt index)
-            |> snd
-            |> List.tail
-            |> computeEvaluation (nestedCalculation.ToString()) prevEval
+            nestedCalculation
+            |> Result.bind (fun nestedResult ->
+                rest
+                |> List.tryFindIndex (fun c -> c = Operator.Signs.CloseBracket)
+                |> Option.map (fun index ->
+                    rest
+                    |> List.splitAt index
+                    |> snd
+                    |> List.tail
+                    |> computeEvaluation (nestedResult.ToString()) prevEval)
+                |> Option.defaultValue (Error "Missing bracket"))
         | Operator.Signs.CloseBracket :: _ -> numberBuffer |> int |> prevEval
         | d :: rest when Char.IsDigit(d) -> computeEvaluation $"{numberBuffer}{d}" prevEval rest
         | _ :: rest -> computeEvaluation numberBuffer prevEval rest
         | [] -> numberBuffer |> int |> prevEval
 
-    computeEvaluation String.Empty id (splitComputation |> Seq.toList)
+    computeEvaluation String.Empty Ok (splitComputation |> Seq.toList)
 
 let result2 = computeCalculation calculation
-printfn "Result is: %i" result2
+
+match result2 with
+| Error error -> $"Error: {error}"
+| Ok ok -> $"Result is: %i{ok}"
+|> printfn "%s"
